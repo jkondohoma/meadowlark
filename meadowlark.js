1b7b1ea619b38c855d3bf5f130d3da001eeab66e
jkondohoma <36901483+jkondohoma@users.noreply.github.com>
2021-11-03 18:38:37 -0500

    archive oage added

--- meadowlark.js
+++ meadowlark.js
@@ -66,7 +66,7 @@ app.get("/about", handlers.about);
 app.get('/newsletter-signup', handlers.newsletterSignup)
 app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
 app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
-
+app.get('/newsletter-archive', handlers.newsletterArchive)
 // handlers for fetch/JSON form submission
 app.get('/newsletter', handlers.newsletter)
 app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
